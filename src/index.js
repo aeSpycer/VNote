e3630f5911ea12a23736c2b18c37aa7be547a85c
Immune ae one <ae.spycer@outlook.com>
2024-04-29 22:29:53 -0600

    Estableciendo archivos base del proyecto.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-
-
-const app = express();
-
-// Middlewares
-
-app.use(morgan('dev'));
-
-app.use("/public", express.static(path.join(__dirname, 'public')));
-app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
-
-// Routes
-
-app.get('/', (req, res) => {
-
-    res.send('Ruta inicial');
-
-});
-
-app.get('/inicio', (req, res) => {
-
-    res.send('Ruta principal');
-
-});
+const app = require('./app.js');
 
 app.listen(3000);
-
-console.log(`server on port ${3000}`);
+console.log('Server on port', 3000);
