93b0f9a00c2b3d880aa9dc7c4e22b06b5be90e02
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 10:10:47 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,17 @@ import taskRoutes from './routes/task.routes.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+
+  res.header("Access-Control-Allow-Origin", "https://vnote-f.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+  next();
+  
+});
+
 app.use(cors({ origin: process.env.FRONTEND_URL || "https://vnote-f.onrender.com", credentials: true}));
 app.use(morgan('dev'));
 app.use(express.json());
