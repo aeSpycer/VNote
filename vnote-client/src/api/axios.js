0d3155bc46593e7a202bde86b0d8c19835350f34
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 10:49:25 -0600

    Update axios.js

--- vnote-client/src/api/axios.js
+++ vnote-client/src/api/axios.js
@@ -3,10 +3,8 @@ import axios from "axios";
 const URL = import.meta.env.VITE_BACKEND_URL || "https://vnote-b.onrender.com";
 
 const instance = axios.create({
-
     baseURL: `${URL}/api`,
     withCredentials: true
-
 });
 
 export default instance;
