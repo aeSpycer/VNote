d7adab9abf4f90826a4eea24fab32f5cb270d8cd
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 11:04:08 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use((req, res, next) => {
 
-  res.header("Access-Control-Allow-Origin", "https://vnote-f.onrender.com");
+  res.header("Access-Control-Allow-Origin", "https://vnote-2cy7.onrender.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true // Habilita el intercambio de cookies entre dominios
 }));
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "https://vnote-f.onrender.com", credentials: true}));
+app.use(cors({ origin: process.env.FRONTEND_URL || "https://vnote-2cy7.onrender.com", credentials: true}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
