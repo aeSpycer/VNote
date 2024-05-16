10d859136a6e865be29dbe099d354a88d0538c04
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 10:20:18 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,6 +19,11 @@ app.use((req, res, next) => {
   
 });
 
+app.use(cors({
+  origin: 'https://vnote-f.onrender.com', // Reemplaza con el dominio de tu frontend
+  credentials: true // Habilita el intercambio de cookies entre dominios
+}));
+
 app.use(cors({ origin: process.env.FRONTEND_URL || "https://vnote-f.onrender.com", credentials: true}));
 app.use(morgan('dev'));
 app.use(express.json());
