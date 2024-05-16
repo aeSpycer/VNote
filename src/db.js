2ab8f6b921b5ef5c9af1bb8c9fba5b39f989b00a
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 08:49:03 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,10 +6,7 @@ export const connectDB = async () => {
 
     try {
 
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log(">>> DB is connected");
 
     } catch (error) { console.log(error); }
