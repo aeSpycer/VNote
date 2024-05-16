7ef8203a0c8bd8fe4081bc7c1caee0ada76da119
André Garibay <107881800+aeSpycer@users.noreply.github.com>
2024-05-16 09:01:00 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://vnote-f.onrender.com";
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://immune_ae_one:PHJV04RNxiHZG51D@vnote.7xyx1pi.mongodb.net/VNote";
 export const PORT = process.env.PORT || 5000;
 
