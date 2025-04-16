111270ba0b73038efab236a6bd8c53e6aa564cf7
Immune ae one <ae.spycer@outlook.com>
2025-04-16 16:08:35 -0600

    actualizando ruta frontend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://aeSpycer.github.io/VNote";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://aeSpycer.github.io";
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://aeSpycer:4wVkOLUeB6iJAt2L@aespycer.arh2hrw.mongodb.net/?retryWrites=true&w=majority&appName=aeSpycer";
 
 export const PORT = process.env.PORT || 5000;
