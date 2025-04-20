86c9d724826bf8326ad4d3db0407e56b42e04754
FloydQ <floydquezada117@gmail.com>
2025-04-19 20:37:07 -0600

    cambie URLs a Locales

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
-
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://aespycer.github.io";
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://aeSpycer:4wVkOLUeB6iJAt2L@aespycer.arh2hrw.mongodb.net/VNote?retryWrites=true&w=majority&appName=aeSpycer";
+export const FRONTEND_URL =
+  process.env.FRONTEND_URL || "http://localhost:5173/";
+export const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://aeSpycer:4wVkOLUeB6iJAt2L@aespycer.arh2hrw.mongodb.net/VNote?retryWrites=true&w=majority&appName=aeSpycer";
 
 export const PORT = process.env.PORT || 5000;
 
-const TOKEN_SECRET = 'super secret key';
+const TOKEN_SECRET = "super secret key";
 
 export { TOKEN_SECRET };
