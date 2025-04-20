79788b9b7e11ed7f2a765c47191c5bea54000c45
Hector OG <59241967+CI5KO@users.noreply.github.com>
2025-04-19 21:02:27 -0600

    branch init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import app from './app.js';
-import { connectDB } from './db.js';
+import app from "./app.js";
+import { connectDB } from "./db.js";
 
 connectDB();
 app.listen(process.env.PORT || 5000);
-console.log('Server on port', process.env.PORT || 5000);
+console.log("Server on port: ", process.env.PORT || 5000);
